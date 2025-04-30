e8ee1b5f2e3fed6c69f8cf750b1dfdd5d472cb2f
Sanufer <Sanufer.ms@gmail.com>
2025-05-01 00:26:57 +0530

    Added new Model classes

---
+++ FootballLeague.Domain/Entities/Player.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FootballLeague.Domain.Entities
+{
+    public class Player
+    {
+        public int PlayerId { get; set; }  // Unique identifier
+
+        [Required]
+        public string Name { get; set; }  = string.Empty;  // Player name
+
+        [Required]
+        public string Position { get; set; }  =  string.Empty; // e.g., Goalkeeper, Defender, Midfielder, Forward
+
+        public int TeamId { get; set; } // Foreign key to Team
+
+        public Teams Teams { get; set; } // Navigation property
+    }
+}
